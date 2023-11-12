1e333e38416b5cddd645ca9642aedb1d63415da3
Isaque Akamine <isaqueakamine@gmail.com>
2023-11-11 23:35:06 -0300

    feat: adding tab bar icons

--- src/routes/HomeTab.tsx
+++ src/routes/HomeTab.tsx
@@ -1,14 +1,68 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Home } from "../screens";
-import HomeStack from "./HomeStack";
+import { Image } from "react-native";
+import colors from "../constants/colors";
 
 const Tab = createBottomTabNavigator();
 
 export default function HomeTab() {
   return (
     <Tab.Navigator
-      screenOptions={{ headerShown: false }}
       initialRouteName="Home"
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarIcon: ({ focused, color, size }) => {
+          if (route.name === "Home") {
+            return (
+              <Image
+                source={require("../assets/icons/Home.png")}
+                style={{
+                  width: 24,
+                  height: 24,
+                  tintColor: focused ? colors.primary : "gray",
+                }}
+              />
+            );
+          } else if (route.name === "Favorite") {
+            return (
+              <Image
+                source={require("../assets/icons/Heart.png")}
+                style={{
+                  width: 24,
+                  height: 24,
+                  tintColor: focused ? colors.primary : "gray",
+                }}
+              />
+            );
+          } else if (route.name === "Profile") {
+            return (
+              <Image
+                source={require("../assets/icons/Profile.png")}
+                style={{
+                  width: 24,
+                  height: 24,
+                  tintColor: focused ? colors.primary : "gray",
+                }}
+              />
+            );
+          } else if (route.name === "Cart") {
+            return (
+              <Image
+                source={require("../assets/icons/Buy.png")}
+                style={{
+                  width: 24,
+                  height: 24,
+                  tintColor: focused ? colors.primary : "gray",
+                }}
+              />
+            );
+          }
+        },
+        tabBarActiveTintColor: colors.primary,
+        tabBarInactiveTintColor: "gray",
+        tabBarStyle: { backgroundColor: "transparent", borderTopWidth: 0 },
+      })}
     >
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Favorite" component={Home} />
