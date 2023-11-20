e8e19343660aa3ed239cd24ebd4a9ffae3153172
Isaque Akamine <isaqueakamine@gmail.com>
2023-11-20 00:01:46 -0300

    feat: adding activity indicator on custom button

--- src/components/CustomButton/index.tsx
+++ src/components/CustomButton/index.tsx
@@ -1,21 +1,31 @@
 import React from "react";
-import { TouchableOpacityProps } from "react-native";
+import { ActivityIndicator, TouchableOpacityProps } from "react-native";
 
 import { Container, Title } from "./styles";
+import colors from "../../constants/colors";
 
 type TCustomButton = TouchableOpacityProps & {
   description: string;
   theme?: "light" | "dark";
+  isLoading?: boolean;
 };
 
 const CustomButton: React.FC<TCustomButton> = ({
   description,
   theme = "light",
+  isLoading = false,
   ...rest
 }) => {
   return (
     <Container {...rest} theme={theme}>
-      <Title theme={theme}>{description}</Title>
+      {isLoading ? (
+        <ActivityIndicator
+          size="small"
+          color={theme === "light" ? colors.primary : "#fff"}
+        />
+      ) : (
+        <Title theme={theme}>{description}</Title>
+      )}
     </Container>
   );
 };
