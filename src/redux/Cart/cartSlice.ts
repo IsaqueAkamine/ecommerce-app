f8ccbb5d3a516f7f92c27956481a9734757a9019
Isaque Akamine <isaqueakamine@gmail.com>
2023-11-23 12:03:30 -0300

    feat: added cart reducer

---
+++ src/redux/Cart/cartSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  products: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const product = action.payload;
+      state.products.push(product);
+    },
+  },
+});
+
+export const { addToCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
+
+export const cartList = (state) => state.cart.products;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./Auth/authSlice";
+import cartReducer from "./Cart/cartSlice";
 import productReducer from "./Products/productSlice";
 
 export const store = configureStore({
-  reducer: { auth: authReducer, products: productReducer },
+  reducer: { auth: authReducer, cart: cartReducer, products: productReducer },
 });
